6ebbbac3fb22ccc794356e784c02ba25b592ff20
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-01-02 18:32:55 +0100

    Initial commit

---
+++ Msploit-X/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Msploit_X.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
