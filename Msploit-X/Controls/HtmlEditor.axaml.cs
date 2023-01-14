dc1e510e82f8e88ef6d5817f3cbeb09e85f1ebe6
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-01-14 14:13:26 +0100

    working text editor #1

--- Msploit-X/Controls/HtmlEditor.axaml.cs
+++ Msploit-X/Controls/HtmlEditor.axaml.cs
@@ -3,19 +3,27 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using AvaloniaEdit;
 using AvaloniaEdit.Indentation.CSharp;
+using AvaloniaEdit.TextMate;
+using TextMateSharp.Grammars;
 
 namespace Msploit_X.Controls
 {
-    public partial class CodeEditor : UserControl
+    public partial class HtmlEditor : UserControl
     {
         private readonly TextEditor _textEditor;
         
-        public CodeEditor()
+        public HtmlEditor()
         {
             InitializeComponent();
             _textEditor = this.FindControl<TextEditor>("textCode");
             _textEditor.ShowLineNumbers = true;
             _textEditor.TextArea.IndentationStrategy = new CSharpIndentationStrategy();
+            
+            var  _registryOptions = new RegistryOptions(ThemeName.DarkPlus);
+
+            var _textMateInstallation = _textEditor.InstallTextMate(_registryOptions);
+
+            _textMateInstallation.SetGrammar(_registryOptions.GetScopeByLanguageId(_registryOptions.GetLanguageByExtension(".html").Id));
         }
 
         private void InitializeComponent()
